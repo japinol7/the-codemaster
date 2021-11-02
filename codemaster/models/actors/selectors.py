8c519bf18419490cbdc0da1f56fea468a2d072b2
japinol <japinol@outlook.com>
2021-11-02 17:47:11 +0100

    [IMP] Improve the performance of rendering light for the magic selector.

--- codemaster/models/actors/selectors.py
+++ codemaster/models/actors/selectors.py
@@ -6,7 +6,7 @@ import logging
 import pygame as pg
 
 
-from codemaster.config.constants import BM_SELECTORS_FOLDER
+from codemaster.config.constants import BM_SELECTORS_FOLDER, DIRECTION_RIP
 from codemaster.models.actors.actor_types import ActorCategoryType, ActorType
 from codemaster.models.actors.actors import ActorItem
 from codemaster.models.stats import Stats
@@ -32,14 +32,19 @@ class Selector(ActorItem):
         self.stats.power = self.stats.power_total = 0
         self.stats.strength = self.stats.strength_total = 1
         self.light_grid = None
+        self.light_grid_surf_size = (34, 34)
+        self.light_grid_surf_size_half = (self.light_grid_surf_size[0] // 2, self.light_grid_surf_size[1] // 2)
         self.light_color = Color.GREEN
 
         super().__init__(x, y, game, name=name)
 
         self._create_light()
 
+    def update_sprite_image(self):
+        pass
+
     def _create_light(self):
-        self.light_grid = LightGrid(self.game.screen.get_size())
+        self.light_grid = LightGrid(self.light_grid_surf_size)
         self.light_grid.add_light(
             Light((0, 0), radius=18, color=self.light_color, alpha=255),
             )
@@ -51,10 +56,22 @@ class Selector(ActorItem):
         if Settings.has_selector_no_light:
             return
 
+        # Create a surface with only the part of the screen that is needed for the light grid render
+        sub_screen_rect = pg.Rect(
+            mx - self.light_grid_surf_size_half[0],
+            my - self.light_grid_surf_size_half[1],
+            self.light_grid_surf_size[0],
+            self.light_grid_surf_size[1])
+        grid_surface = pg.Surface(sub_screen_rect.size)
+        grid_surface.blit(self.game.screen, (0, 0), sub_screen_rect)
+
+        # Render all the lights of the light grid
         for light in self.light_grid.lights.values():
             light.set_color(self.light_color, override_alpha=True)
-            light.position = (mx, my)
-        self.light_grid.render(self.game.screen)
+            light.position = (self.light_grid_surf_size_half[0],
+                              self.light_grid_surf_size_half[1])
+        self.light_grid.render(grid_surface)
+        self.game.screen.blit(grid_surface, sub_screen_rect)
 
     def update_when_hit(self):
         """Cannot be hit."""
@@ -62,6 +79,9 @@ class Selector(ActorItem):
 
     def get_pointed_sprites(self):
         hit_list = []
+
+        if self.player.direction == DIRECTION_RIP:
+            return hit_list
 
         if logging.getLevelName(logger.level) == 'DEBUG':
             hit_list = pg.sprite.spritecollide(self, self.game.level.all_sprites, False)
