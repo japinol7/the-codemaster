9a166ff379fe81e36a53ba45f0a2e32dce5b24c5
japinol <japinol@outlook.com>
2023-04-15 02:52:05 +0200

    [IMP] Remove unnecessary stuff from the current version.

--- codemaster/models/stats.py
+++ codemaster/models/stats.py
@@ -23,40 +23,8 @@ class Stats:
         self.strength = None
         self.strength_total = None
         self.speed = None
-        self.breath_elements = []  # List of elements
         self.time_between_shots = None
         self.energy_shield = None
-        # Defence
-        self.defense_melee = None
-        self.defense_missile = None
-        self.defense_thrown = None
-        self.defense_breathes = []  # List of elements
-        self.shield_bonus = None
-        # Resistance against poisons, mental control, etc
-        self.resistance_poison = None
-        self.resistance_mental = None
-        self.resistance_elements = []  # List of elements
-        self.resistance_magic = []  # List of magic sources
-        # Attacks
-        self.melee = None
-        self.missile = None
-        self.thrown = None
-        self.breath = None
-        # Attack modifiers
-        self.poisoned = None
-        self.elemental = None
-
-    def add_breath_element(self, element):
-        self.breath_elements.append(element)
-
-    def add_defense_breaths(self, element):
-        self.defense_breathes.append(element)
-
-    def add_resistance_elements(self, element):
-        self.resistance_elements.append(element)
-
-    def add_resistance_magic(self, element):
-        self.resistance_magic.append(element)
 
 
 class NPCStats(Stats):
